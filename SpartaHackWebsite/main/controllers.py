4670cd63ef48683d82a9892cd879a4040049313a
Yash Sharma <tellsharmayash@gmail.com>
2017-11-16 18:09:24 -0500

    some updates to use config file

--- SpartaHackWebsite/main/controllers.py
+++ SpartaHackWebsite/main/controllers.py
@@ -3,6 +3,8 @@ from flask import Flask, request, render_template
 import requests
 import json
 import operator
+from flask import current_app as app
+
 main = Blueprint('main', __name__,template_folder='templates')
 
 
@@ -11,7 +13,7 @@ def get_faqs():
 
 	headers = {
 		'content-type': "application/json",
-		'x-www-user-token': "UZb6Ryd2NYgPVXnT_xHL"
+		'authorisation':app.config['DEV_API_KEY']
 		}
 	faqs=[]
 	response = requests.request("GET", url, headers=headers)
@@ -21,15 +23,23 @@ def get_faqs():
 			temp=(item.get('question'),item.get('answer'),item.get('priority'))
 			faqs.append(temp)
 	faqs.sort(key=operator.itemgetter(2))
+	temp=None
 	return faqs
 
 	
 def get_sponsors():
 	url = "https://api.elephant.spartahack.com/sponsors/"
-	
+	headers = {
+		'content-type': "application/json",
+		'authorisation':app.config['DEV_API_KEY']
+		}
+	sponsors=[]
+	#response = requests.request("GET", url, headers=headers)
+	#temp=json.loads(response.text)
+	return sponsors
 @main.route('/')
 def index():
 	logged_in=None
 	faqs=get_faqs()
 	sponsors=get_sponsors()
-	return render_template("index.html",faqs=faqs,logged_in=logged_in	)
+	return render_template("index.html",faqs=faqs,logged_in=logged_in,sponsors=sponsors	)

cd168ebd75d2f57ff80b80ae03573f23e00c27e1
Yash Sharma <tellsharmayash@gmail.com>
2017-11-17 21:08:08 -0500

    adds login and sponsors on the index. The html looks ugly. Need to work with @harris about adding graphics and stuff. Let me know if something breaks. Right now there are just two accounts on dev api. Contact @bogdan about creds for them

--- SpartaHackWebsite/dashboard/controllers.py
+++ SpartaHackWebsite/dashboard/controllers.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 
 dashboard = Blueprint('dashboard', __name__,template_folder='templates')
 
 
 @dashboard.route('/')
-def index():
-    return render_template("dashboard.html")
+def index():		
+	if session.get('auth_token') is None:		#if user not logged in then redirect to login page
+		return redirect('/login')
+	return render_template("dashboard.html")
 
 	

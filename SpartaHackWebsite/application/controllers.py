06526449ff7b354ac32cc7f0d31613936c8b986f
Yash Sharma <tellsharmayash@gmail.com>
2017-11-22 07:03:47 -0500

    testing git integration with atom

--- SpartaHackWebsite/application/controllers.py
+++ SpartaHackWebsite/application/controllers.py
@@ -7,11 +7,11 @@ application = Blueprint('application', __name__,template_folder='templates')
 @application.route('/')
 def index():
     return render_template("application.html")
-	
+
 @application.route('/validateSignUp')
 def validateSignUp():
 	pass
-	
+
 @application.route('/edit')
 def edit():
 	return render_template("edit.html")
